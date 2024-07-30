ce3bfbd9cf617207f7d562b5fa56ac7b3f688e7f
Preethi R <169124377+PREETHI-R15@users.noreply.github.com>
2024-07-30 20:20:15 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class ATM {
+
+    public static void main(String[] args) throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+        introduction();
+        optionMenu.mainMenu();
+    }
+    public static void introduction(){
+        System.out.println("WELCOME TO THE ATM PROJECT! ");
+    }
+}
